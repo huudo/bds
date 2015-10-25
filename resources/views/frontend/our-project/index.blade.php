72d8fb5693afe82d7e70a749d1c9ffc70b538997
Huu <huudt.3012@gmail.com>
2015-10-25 15:04:58 +0700

    Upload file

---
+++ resources/views/frontend/our-project/index.blade.php
@@ -0,0 +1,156 @@
+@extends('layouts.frontend')
+
+@section('content')
+  <div class="site-content">
+  <div class="big-title" style="background-image: url('../data/breadcrumb_bg.jpg')">
+    <div class="container">
+      <h1 class="entry-title">Thiết kế nội thất</h1>             <div class="breadcrumb">
+          <div class="container">
+            <ul class="tm_bread_crumb">
+              <li class="level-1 top"><a href="trang_chu">Trang chủ</a></li>
+              <li class="level-2 sub tail current">Thiết kế nội thất</li>
+            </ul>
+          </div>
+        </div>
+          </div>
+  </div>
+<div class="container" style="padding-bottom:50px;">
+
+  <div class="menu_new" style="text-align:center;">
+      <nav class="navbar navbar-default" style="width:100%;text-align:center;">
+          
+          <ul id="myTab" class="nav navbar-nav" style="display: inline-block;">
+            <li class="active"><a href="#all" data-toggle="tab">Tất cả</a></li>
+            <li class=""><a href="#tk_noi_that" data-toggle="tab">Thiết kế nội thất</a></li>
+            <li class=""><a href="#tk_noi_that" data-toggle="tab">Thi công nội thất</a></li>
+            <li class=""><a href="#tk_noi_that" data-toggle="tab">Thiết kế kiến trúc</a></li> 
+          </ul>
+          
+      </nav>
+  </div>
+  <div id="myTabContent" class="tab-content">
+    <div class="tab-pane fade active in" id="all">
+
+     <div class="row">   
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/01.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+      <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+     <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/02.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+       <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/04.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+       <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/02.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+       <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/01.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+       <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/02.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+       <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/04.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+
+      <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+      </div>
+      <div class="col-md-3 project" style="margin-top:20px; text-align:center">
+   
+      <a href="http://www.flickr.com/photos/anirudhkoul/3536413126/">
+        <figure>
+          <img src="/wp-content/uploads/2015/07/02.jpg" width="100%" height="auto" alt="Arc de Triomphe">
+          <figcaption><p>Arc de Triomphe</p></figcaption>
+        </figure>
+      </a>
+       <p style="color: #7c795d; font-family: 'Trocchi', serif; font-size: 20px; font-weight: normal; line-height: 48px; margin: 0;">Arc de Triomphe</p>
+    
+      </div>
+
+      
+
+     </div>
+    </div>
+    <div class="tab-pane fade" id="tk_noi_that">
+           <div class="row">
+      <div class="col-md-3" style="margin-top:20px">
+          <img src="/wp-content/uploads/2015/07/01.jpg" style=" max-width: 100%; 
+      min-height: 300px;">
+      </div>
+     <div class="col-md-3" style="margin-top:20px">
+          <img src="/wp-content/uploads/2015/07/02.jpg" style=" max-width: 100%; 
+      min-height: 300px;">
+      </div>
+      <div class="col-md-3" style="margin-top:20px">
+          <img src="/wp-content/uploads/2015/07/02.jpg" style=" max-width: 100%; 
+      min-height: 300px;">
+      </div>
+     <div class="col-md-3" style="margin-top:20px">
+          <img src="/wp-content/uploads/2015/07/04.jpg" style=" max-width: 100%; 
+      min-height: 300px;">
+      </div>
+      
+
+     </div>
+    </div>
+
+  </div>
+</div>
+
+</div><!-- #content -->
+
+@stop
